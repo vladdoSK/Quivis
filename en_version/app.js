3ed877c3e22fa8d9dd7012f0c59a632cd75a5319
Vladyslav <106543899+vladdoSK@users.noreply.github.com>
2023-01-09 17:42:44 +0200

    update js files

--- en_version/app.js
+++ en_version/app.js
@@ -1,58 +1,61 @@
-const animation__title = document.querySelector('.animation__title');
-setTimeout(() => {
-    animation__title.classList.add("active");
-}, 300)
+document.addEventListener("DOMContentLoaded", function () {
 
-setTimeout(() => {
-    animation__title.classList.remove("active");
-}, 9000)
+    const animation__title = document.querySelector('.animation__title');
+    setTimeout(() => {
+        animation__title.classList.add("active");
+    }, 300)
 
-//---------------------------------------------------------
-//---------------------------------------------------------
+    setTimeout(() => {
+        animation__title.classList.remove("active");
+    }, 9000)
 
-const video = document.querySelector(".video__item");
-const sub_video_block = document.querySelector('.sup__video');
+    //---------------------------------------------------------
+    //---------------------------------------------------------
 
-let mas_video = ["../video/analize_traffic.mp4",
-    "../video/security.mp4",
-    "../video/restricted_areas.mp4",
-    "../video//analize_traffic.webm",
-    "../video/security.webm",
-    "../video/restricted_areas.webm"];
+    const video = document.querySelector(".video__item");
+    const sub_video_block = document.querySelector('.sup__video');
 
-let sup__video = ["traffic analysts",
-    "security agencies",
-    "restricted areas"]
+    let mas_video = ["../video/analize_traffic.mp4",
+        "../video/security.mp4",
+        "../video/restricted_areas.mp4",
+        "../video//analize_traffic.webm",
+        "../video/security.webm",
+        "../video/restricted_areas.webm"];
 
-let i_video = 0;
+    let sup__video = ["traffic analysts",
+        "security agencies",
+        "restricted areas"]
 
-setInterval(() => {
-    ++i_video;
-    if (i_video != 3) {
-        sub_video_block.innerHTML = `Solution for <span class="blue__text solution__for">${sup__video[i_video]}</span>
+    let i_video = 0;
+
+    setInterval(() => {
+        ++i_video;
+        if (i_video != 3) {
+            sub_video_block.innerHTML = `Solution for <span class="blue__text solution__for">${sup__video[i_video]}</span>
                                 <div class="animation__title"></div>`
-    }
-    video.innerHTML = `<video muted autoplay loop src="${mas_video[i_video]}">
-                            <source src="${mas_video[i_video+3]}" type="video/webm">
+        }
+        video.innerHTML = `<video muted autoplay loop src="${mas_video[i_video]}">
+                            <source src="${mas_video[i_video + 3]}" type="video/webm">
                             <source src="${mas_video[i_video]}" type="video/mp4">
                         </video>`
-    const anim__solution = document.querySelector('.solution__for');
+        const anim__solution = document.querySelector('.solution__for');
 
-    if (i_video != 3) {
-        setTimeout(() => {
-            const animation__title = document.querySelector('.animation__title');
-            animation__title.classList.add("active");
-        }, 300)
-    }
+        if (i_video != 3) {
+            setTimeout(() => {
+                const animation__title = document.querySelector('.animation__title');
+                animation__title.classList.add("active");
+            }, 300)
+        }
 
-    if (i_video != 2) {
-        setTimeout(() => {
-            const animation__title = document.querySelector('.animation__title');
-            animation__title.classList.remove("active");
-        }, 9000)
-    }
+        if (i_video != 2) {
+            setTimeout(() => {
+                const animation__title = document.querySelector('.animation__title');
+                animation__title.classList.remove("active");
+            }, 9000)
+        }
 
-    if (i_video == 3) {
-        i_video = -1;
-    }
-}, 10000)
+        if (i_video == 3) {
+            i_video = -1;
+        }
+    }, 10000)
+});
--- js/app.js
+++ js/app.js
@@ -1,71 +1,73 @@
-const animation__title = document.querySelector('.animation__title');
-setTimeout(() => {
-    animation__title.classList.add("active");
-}, 300)
+document.addEventListener("DOMContentLoaded", function () {
 
-setTimeout(() => {
-    animation__title.classList.remove("active");
-}, 9000)
-
-//---------------------------------------------------------
-//---------------------------------------------------------
-
-
-const video = document.querySelector(".video__item");
-const sub_video_block = document.querySelector('.sup__video');
-
-let mas_video = ["./video/analize_traffic.mp4",
-    "./video/security.mp4",
-    "./video/restricted_areas.mp4",
-    "./video//analize_traffic.webm",
-    "./video/security.webm",
-    "./video/restricted_areas.webm"];
-
-let sup__video = ["аналітики трафіку",
-    "охоронних агенцій",
-    "територій з обмеженим доступом"]
-
-let i_video = 0;
-
-setInterval(() => {
-    ++i_video;
-
-    sub_video_block.innerHTML = `Рішення для <span class="blue__text solution__for">${sup__video[i_video]}</span>
-                                <div class="animation__title"></div>`
-    video.innerHTML = `<video muted autoplay loop>
-                            <source src="${mas_video[i_video+3]}" type="video/webm">
-                            <source src="${mas_video[i_video]}" type="video/mp4">
-                        </video>`
-    const anim__solution = document.querySelector('.solution__for');
-
-
+    const animation__title = document.querySelector('.animation__title');
     setTimeout(() => {
-        const animation__title = document.querySelector('.animation__title');
         animation__title.classList.add("active");
     }, 300)
 
     setTimeout(() => {
-        const animation__title = document.querySelector('.animation__title');
         animation__title.classList.remove("active");
     }, 9000)
 
-
-    if (i_video == 2) {
-        i_video = -1;
-    }
-}, 10000)
+    //---------------------------------------------------------
+    //---------------------------------------------------------
 
 
-window.addEventListener("click", (e) => {
-    if(e.target.classList.contains("description__block") || e.target.parentElement.classList.contains("description__block") || e.target.parentElement.parentElement.parentElement.classList.contains("description__block")){
-        if(e.target.className === "description__block"){
-            e.target.classList.toggle("active");
-        } else if(e.target.parentElement.classList.contains("description__block")){
-            e.target.parentElement.classList.toggle("active");
-        } else{
-            e.target.parentElement.parentElement.parentElement.classList.toggle("active");
+    const video = document.querySelector(".video__item");
+    const sub_video_block = document.querySelector('.sup__video');
+
+    let mas_video = ["./video/analize_traffic.mp4",
+        "./video/security.mp4",
+        "./video/restricted_areas.mp4",
+        "./video//analize_traffic.webm",
+        "./video/security.webm",
+        "./video/restricted_areas.webm"];
+
+    let sup__video = ["аналітики трафіку",
+        "охоронних агенцій",
+        "територій з обмеженим доступом"]
+
+    let i_video = 0;
+
+    setInterval(() => {
+        ++i_video;
+
+        sub_video_block.innerHTML = `Рішення для <span class="blue__text solution__for">${sup__video[i_video]}</span>
+                                <div class="animation__title"></div>`
+        video.innerHTML = `<video muted autoplay loop>
+                            <source src="${mas_video[i_video + 3]}" type="video/webm">
+                            <source src="${mas_video[i_video]}" type="video/mp4">
+                        </video>`
+        const anim__solution = document.querySelector('.solution__for');
+
+
+        setTimeout(() => {
+            const animation__title = document.querySelector('.animation__title');
+            animation__title.classList.add("active");
+        }, 300)
+
+        setTimeout(() => {
+            const animation__title = document.querySelector('.animation__title');
+            animation__title.classList.remove("active");
+        }, 9000)
+
+
+        if (i_video == 2) {
+            i_video = -1;
         }
-    }
-})
+    }, 10000)
 
 
+    window.addEventListener("click", (e) => {
+        if (e.target.classList.contains("description__block") || e.target.parentElement.classList.contains("description__block") || e.target.parentElement.parentElement.parentElement.classList.contains("description__block")) {
+            if (e.target.className === "description__block") {
+                e.target.classList.toggle("active");
+            } else if (e.target.parentElement.classList.contains("description__block")) {
+                e.target.parentElement.classList.toggle("active");
+            } else {
+                e.target.parentElement.parentElement.parentElement.classList.toggle("active");
+            }
+        }
+    })
+
+});
